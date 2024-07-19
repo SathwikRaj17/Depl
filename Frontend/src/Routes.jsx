76ee79e75dd6e145d130c8ee3946221476197a3e
Sathwik Raj <sathwikraj003@gmail.com>
2024-07-19 11:35:35 +0530

    Correction in Routes

--- Frontend/src/Routes.jsx
+++ Frontend/src/Routes.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Stream from './Pages/Stream'
-import Results from './Pages/Results';
 const RoutesComponent = () => {
     return (
         <Router>
@@ -10,7 +9,6 @@ const RoutesComponent = () => {
                 <Route path="/error" element={<h1>Sorry Backend is down</h1>} />
                 <Route path="/check" element={<h1>Testing</h1>}/>
                 <Route path="/stream" element={<Stream />} />
-                <Route path="/results" element={<Results />}/>
             </Routes>
         </Router>
     );
